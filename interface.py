c738e365fb99e53c732299c41069e89e75c63010
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-08-25 22:52:01 +0800

    Upload SPRITE draft to Github as i am going to bed once i upload the SPRITE topic so i better get that out of the way

--- interface.py
+++ interface.py
@@ -2,9 +2,39 @@ import scratchattach as scr
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+def with_offset(function):
+    offset = 0
+    result = []
+    while True:
+        returned = function(offset=offset)
+        if not returned:
+            break
+        result += returned
+        offset += 40
+    return result
+
+def load_user_data(username):
+    user = scr.get_user(username)
+    followers = with_offset(user.followers)
+    following = with_offset(user.following)
+    comment_count = len(user.comments())
+    projects = with_offset(user.projects)
+
+    return user, {
+        "followers": followers,
+        "following": following,
+        "comment_count": comment_count,
+        "projects": projects
+    }
+
+@app.route("/users/<username>")
+def user(username):
+	user, info = load_user_data(username)
+	return render_template("user.html", user=user, info=info, len=len)
+
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("root.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
